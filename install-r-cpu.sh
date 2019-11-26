cbd0df662c9130353f0cace865d17bde24bf9c50
Zain Rizvi <ZainRizvi@users.noreply.github.com>
2019-11-25 19:38:13 -0800

    Move rpy2 installation to bottom

--- install-r-cpu.sh
+++ install-r-cpu.sh
@@ -1,29 +1,4 @@
 #!/bin/bash
-function install_pip2_package() {
-    pip2 install --upgrade --upgrade-strategy only-if-needed --force-reinstall "$@" || exit 1
-}
-
-function install_pip3_package() {
-    pip3 install --upgrade --upgrade-strategy only-if-needed --force-reinstall "$@" || exit 1
-}
-
-function install_pip_package() {
-    install_pip2_package "$1"
-    install_pip3_package "$1"
-}
-
-# Install rpy2
-
-# To invoke R code in a python notebook, run the following code in a cell:
-#   import rpy2.robjects as robjects
-#   import rpy2.robjects.lib.ggplot2 as ggplot2
-#   %load_ext rpy2.ipython
-#
-# Then you can use the %R and %%R magic commands to run R code
-
-install_pip_package tzlocal # required by rpy2
-# 3.0.5 is the last version that works with Python 3.5
-install_pip3_package rpy2==3.0.5 # Code in both Python & R at the same time
 
 # Install R
 
@@ -107,6 +82,32 @@ mkdir -p "${R_LIBS_USER}"
 EOF
 
 chmod +x "${R_LIB_SETUP}"
+
+function install_pip2_package() {
+    pip2 install --upgrade --upgrade-strategy only-if-needed --force-reinstall "$@" || exit 1
+}
+
+function install_pip3_package() {
+    pip3 install --upgrade --upgrade-strategy only-if-needed --force-reinstall "$@" || exit 1
+}
+
+function install_pip_package() {
+    install_pip2_package "$1"
+    install_pip3_package "$1"
+}
+
+# Install rpy2
+
+# To invoke R code in a python notebook, run the following code in a cell:
+#   import rpy2.robjects as robjects
+#   import rpy2.robjects.lib.ggplot2 as ggplot2
+#   %load_ext rpy2.ipython
+#
+# Then you can use the %R and %%R magic commands to run R code
+
+install_pip_package tzlocal # required by rpy2
+# 3.0.5 is the last version that works with Python 3.5
+install_pip3_package rpy2==3.0.5 # Code in both Python & R at the same time
 
 # Reboot so that R user-installed packages path change takes effect
 sudo reboot
