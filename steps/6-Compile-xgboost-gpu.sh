bd646185b46d96091fb0968a0bab61b9064174da
Zain Rizvi <zainr@google.com>
2019-12-10 11:40:12 -0800

    Remove sudos (not needed)

--- steps/6-Compile-xgboost-gpu.sh
+++ steps/6-Compile-xgboost-gpu.sh
@@ -5,11 +5,11 @@ wget https://github.com/Kitware/CMake/releases/download/v3.16.0-rc2/cmake-3.16.0
 
 chmod +x cmake-3.16.0-rc2-Linux-x86_64.sh
 CMAKE_DIR=/opt/cmake-custom
-sudo mkdir $CMAKE_DIR
-sudo ./cmake-3.16.0-rc2-Linux-x86_64.sh --skip-license --prefix=$CMAKE_DIR --exclude-subdir
+mkdir $CMAKE_DIR
+./cmake-3.16.0-rc2-Linux-x86_64.sh --skip-license --prefix=$CMAKE_DIR --exclude-subdir
 rm cmake-3.16.0-rc2-Linux-x86_64.sh
 
-sudo ln -s $CMAKE_DIR/bin/* /usr/local/bin
+ln -s $CMAKE_DIR/bin/* /usr/local/bin
 
 # Install xgboost
 cd
@@ -19,5 +19,5 @@ mkdir build
 cd build
 cmake -DUSE_CUDA=ON -DR_LIB=ON -DR_LIB=ON -DUSE_NCCL=ON -DCUDA_TOOLKIT_ROOT_DIR=/usr/local/cuda-10.1 -DNCCL_ROOT=/usr/local/nccl2 ..
 
-sudo make -j4
-sudo make install
+make -j4
+make install
